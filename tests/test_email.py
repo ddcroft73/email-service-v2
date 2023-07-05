6d2e8fbbcf908d3aadfa612e2ddade76267ab855
ddcroft73 <gen.disarray73@outlook.com>
2023-07-04 20:45:27 -0400

    Added test file, adjusted Dockerfile.

--- tests/test_email.py
+++ tests/test_email.py
@@ -1,4 +1,12 @@
-import os, sys, time, requests
+
+# I'm currently learning how to launch tests from within the container. My testing till now has consisted
+# of a loop firing as many requests as I want at the endpoint, and making the taskstake random amounts of time.
+# This little service can take it. The creator of fastAPI and creators of Celery have been teaching me a lot.
+
+# This is a standalone script that should be ran in another containre or in its own venv. I tested it by runing 
+# this script from inside V1.
+
+import os, sys, time, requests, asyncio, aiohttp
 from dotenv import load_dotenv
 
 from fastapi.responses import JSONResponse
@@ -28,27 +36,42 @@ email = Email(
 load_dotenv()
 
 def test_email(token):
-    url = 'http://localhost:8014/send-email/'
+    url = 'http://0.0.0.0:8014/send-email/'
 
     headers = {
         'Authorization': f'Bearer {token}',
         'Content-Type': 'application/json'
     }
     
-    for _ in range(1):
-        time.sleep(1)
-
-        response = requests.request(
+    #for _ in range(1):
+    ##    time.sleep(1)
+    
+    response = requests.request(
             "POST", 
             url, headers=headers, 
             json=email.dict()
         )
 
-        print(response.json())
+    print(response.json())
 
 
+async def test_root():
+    url = 'http://localhost:8014/'
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.json()
+
+async def call_test():
 
 
-token:  str = create_token() #'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlRlc3R0aGVTZXJ2aWNlIiwiZXhwIjoxNjg4MjU0NjUwLCJpc3MiOiJ5b3VyLWlzc3VlciIsInN1YiI6InVzZXItMTIzNDUifQ.8bZgW0xObI7HePQkbeMqVktjSsioHdicKM8u72sCatY' #reate_token()
-print(token)
+    res = await test_root()
+    print(res)
+
+#asyncio.run(call_test())
+
+
+token:  str = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlRlc3R0aGVTZXJ2aWNlIiwiZXhwIjoxNjg5MTA3NDg0LCJpc3MiOiJ5b3VyLWlzc3VlciIsInN1YiI6InVzZXItMTIzNDUifQ.Pm9jJ_g0L6pMtYLDbDnSuSK0wQ3hyT3O-IskBZ0Gd7g'#create_token()
+#print(token)
 test_email(token)
+
